0c686f946de865f27dca539bdf9b5497ba812b00
Changhyun Kwon <chkwon@gmail.com>
2015-02-24 14:30:09 -0500

    runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,11 +4,15 @@ using Base.Test
 # write your own tests here
 
 data_time = time()
-# ta_data = load_ta_network("Sioux Falls")
+ta_data = load_ta_network("Sioux Falls")
 # ta_data = load_ta_network("Anaheim")
 # ta_data = load_ta_network("Barcelona")
 # ta_data = load_ta_network("Chicago Sketch")
-ta_data = load_ta_network("Winnipeg")
+# ta_data = load_ta_network("Winnipeg")
 println("Data Loading Completed, time:", time() - data_time, " seconds")
 
 link_flow, link_travel_time, objective = ta_frank_wolfe(ta_data, method=:bfw, log=:on, tol=1e-3, max_iter_no=5)
+
+link_flow, link_travel_time, objective = ta_frank_wolfe(ta_data, method=:cfw, log=:on, tol=1e-3, max_iter_no=5)
+
+link_flow, link_travel_time, objective = ta_frank_wolfe(ta_data, method=:fw, log=:on, tol=1e-3, max_iter_no=5)
