81614fdf2e88771f09d03de75e7848f71e75448d
Changhyun Kwon <chkwon@gmail.com>
2015-02-24 19:25:20 -0500

    commented out some unused functions

--- src/misc.jl
+++ src/misc.jl
@@ -15,40 +15,40 @@ function create_graph(start_node, end_node)
     return graph
 end
 
-function get_shortest_path(start_node, end_node, link_length, origin, destination)
-    @assert Base.length(start_node)==Base.length(end_node)
-    @assert Base.length(start_node)==Base.length(link_length)
-
-    graph = create_graph(start_node, end_node)
-
-    state = dijkstra_shortest_paths(graph, link_length, origin)
-
-    path = get_path(state, origin, destination)
-    x = get_vector(path, start_node, end_node)
-
-    return path, x
-end
-
-function get_vector(state, origin, destination, start_node, end_node)
-    current = destination
-    parent = -1
-    x = zeros(Int, Base.length(start_node))
-
-    while parent != origin
-        parent = state.parents[current]
-
-        for j=1:Base.length(start_node)
-            if start_node[j]==parent && end_node[j]==current
-                x[j] = 1
-                break
-            end
-        end
-
-        current = parent
-    end
-
-    return x
-end
+# function get_shortest_path(start_node, end_node, link_length, origin, destination)
+#     @assert Base.length(start_node)==Base.length(end_node)
+#     @assert Base.length(start_node)==Base.length(link_length)
+#
+#     graph = create_graph(start_node, end_node)
+#
+#     state = dijkstra_shortest_paths(graph, link_length, origin)
+#
+#     path = get_path(state, origin, destination)
+#     x = get_vector(path, start_node, end_node)
+#
+#     return path, x
+# end
+#
+# function get_vector(state, origin, destination, start_node, end_node)
+#     current = destination
+#     parent = -1
+#     x = zeros(Int, Base.length(start_node))
+#
+#     while parent != origin
+#         parent = state.parents[current]
+#
+#         for j=1:Base.length(start_node)
+#             if start_node[j]==parent && end_node[j]==current
+#                 x[j] = 1
+#                 break
+#             end
+#         end
+#
+#         current = parent
+#     end
+#
+#     return x
+# end
 
 
 function get_vector(state, origin, destination, link_dic)
