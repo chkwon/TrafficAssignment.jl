97112a030c52e5543f3b43c0f8d187a115c4a856
Changhyun Kwon <chkwon@usf.edu>
2016-07-26 17:00:44 -0400

    include files before exporting methods

--- src/TrafficAssignment.jl
+++ src/TrafficAssignment.jl
@@ -4,13 +4,14 @@ module TrafficAssignment
 using LightGraphs, Optim
 
 
+include("load_network.jl")
+include("frank_wolfe.jl")
+
+
 export
         load_ta_network,
         ta_frank_wolfe
 
 
-
-include("load_network.jl")
-include("frank_wolfe.jl")
 
 end # module
