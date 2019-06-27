47220b63983e5392bf7c2c1dbfec1ab3d277c63c
Changhyun Kwon <chkwon@usf.edu>
2019-06-27 10:54:47 -0400

    testing complementarity

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,18 +4,28 @@ using Test
 include("test_functions.jl")
 
 
-test_tntp()
+# test_tntp()
 
-summarize_ta_data()
+# summarize_ta_data()
 
 data_time = time()
 ta_data = load_ta_network("SiouxFalls")
 
 println("Data Loading Completed, time:", time() - data_time, " seconds")
 
-link_flow, link_travel_time, objective = ta_frank_wolfe(ta_data, method=:bfw, step=:exact, log=:on, tol=1e-3, max_iter_no=5)
 
-link_flow, link_travel_time, objective = ta_frank_wolfe(ta_data, method=:cfw, step=:newton, log=:on, tol=1e-3, max_iter_no=5)
+link_flow, link_travel_time, objective = ta_frank_wolfe(ta_data, method=:bfw, step=:exact, log=:on, tol=1e-6, max_iter_no=100)
 
-link_flow, link_travel_time, objective = ta_frank_wolfe(ta_data, method=:fw, step=:exact, log=:on, tol=1e-3, max_iter_no=5)
-@assert abs( objective - 4.963799502172654e6 ) < 1e6
+@show objective
+
+link_flow2, link_travel_time2, objective2 = ta_nlp(ta_data, method=:bfw, step=:exact, log=:on, tol=1e-6, max_iter_no=100)
+
+
+@show objective2
+
+# link_flow, link_travel_time, objective = ta_frank_wolfe(ta_data, method=:bfw, step=:exact, log=:on, tol=1e-3, max_iter_no=5)
+#
+# link_flow, link_travel_time, objective = ta_frank_wolfe(ta_data, method=:cfw, step=:newton, log=:on, tol=1e-3, max_iter_no=5)
+#
+# link_flow, link_travel_time, objective = ta_frank_wolfe(ta_data, method=:fw, step=:exact, log=:on, tol=1e-3, max_iter_no=5)
+# @assert abs( objective - 4.963799502172654e6 ) < 1e6
