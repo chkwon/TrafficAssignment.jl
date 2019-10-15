c9439fea621e2d0afbca1b985cbcd7a3fc644485
Changhyun Kwon <chkwon@usf.edu>
2019-10-15 15:19:07 -0400

    solution tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,7 @@
 using TrafficAssignment
-using Test
+using Test, LinearAlgebra
+using DelimitedFiles
+
 
 include("test_functions.jl")
 
@@ -10,12 +12,35 @@ summarize_ta_data()
 
 data_time = time()
 ta_data = load_ta_network("SiouxFalls")
-
 println("Data Loading Completed, time:", time() - data_time, " seconds")
 
-link_flow, link_travel_time, objective = ta_frank_wolfe(ta_data, method=:bfw, step=:exact, log=:on, tol=1e-3, max_iter_no=5)
 
-link_flow, link_travel_time, objective = ta_frank_wolfe(ta_data, method=:cfw, step=:newton, log=:on, tol=1e-3, max_iter_no=5)
+@testset "Various FW Methods" begin
+    ta_data = load_ta_network("SiouxFalls")
 
-link_flow, link_travel_time, objective = ta_frank_wolfe(ta_data, method=:fw, step=:exact, log=:on, tol=1e-3, max_iter_no=5)
-@assert abs( objective - 4.963799502172654e6 ) < 1e6
+    link_volume, link_travel_time, objective = ta_frank_wolfe(ta_data, method=:bfw, step=:exact, log=:on, tol=1e-3, max_iter_no=5)
+    @test abs( objective - 4.963799502172654e6 ) < 1e6
+
+    link_volume, link_travel_time, objective = ta_frank_wolfe(ta_data, method=:cfw, step=:newton, log=:on, tol=1e-3, max_iter_no=5)
+    @test abs( objective - 4.963799502172654e6 ) < 1e6
+
+    link_volume, link_travel_time, objective = ta_frank_wolfe(ta_data, method=:fw, step=:exact, log=:on, tol=1e-3, max_iter_no=5)
+    @test abs( objective - 4.963799502172654e6 ) < 1e6
+end
+
+@testset "Sioux Falls" begin
+    ta_data = load_ta_network("SiouxFalls")
+    link_volume, link_travel_time, objective = ta_frank_wolfe(ta_data, method=:cfw, step=:newton, log=:on, tol=1e-5, max_iter_no=50000)
+    solution, header = readdlm("SiouxFalls_flow.csv", ',', header=true)
+    solution_flow = Float64.(solution[:, 3])
+    @test norm(link_volume - solution_flow) / norm(solution_flow) < 0.01
+end
+
+
+@testset "Anaheim" begin
+    ta_data = load_ta_network("Anaheim")
+    link_volume, link_travel_time, objective = ta_frank_wolfe(ta_data, method=:cfw, step=:newton, log=:on, tol=1e-5, max_iter_no=50000)
+    solution, header = readdlm("Anaheim_flow.csv", ',', header=true)
+    solution_flow = Float64.(solution[:, 3])
+    @test norm(link_volume - solution_flow) / norm(solution_flow) < 0.01
+end
